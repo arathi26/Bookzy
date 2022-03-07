8cdc8ab4b6bee16f6d932fc8fba3f1f28b9e6b63
Haytham Osman <osman.online@yahoo.com>
2022-03-07 19:54:40 +0300

    First Commit On Bookzy

---
+++ src/main/java/Simplilearn/Bookzy/BookzyApplication.java
@@ -0,0 +1,23 @@
+package Simplilearn.Bookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+
+public class BookzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return " WELCOME TO SIMPLILEARN BOOKZY ONLINE BOOK SHOOP ";
+
+
+	}
+}
---
+++ src/test/java/Simplilearn/Bookzy/BookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package Simplilearn.Bookzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
